cc2a55a182a4c2fbfd980a04338f42b335ad0795
aidanho1188 <saidanho@yccc.edu>
2018-10-31 19:35:11 -0400

    10/31/2018

--- src/edu/ho/java/hangman_v2/Controller.java
+++ src/edu/ho/java/hangman_v2/Controller.java
@@ -10,60 +10,60 @@ import java.util.Scanner;
 
 public class Controller {
 
-	public Controller() 
-	{
+	public Controller() {
 
 	}
-	public List<Game> read() throws IOException
-	{
+
+	public List<Game> read() throws IOException {
 		/**
 		 * Control how the game procedure
 		 */
 		Scanner user_input = new Scanner(System.in);
 		List<Game> games = new ArrayList<Game>();
 		File f = new File("GameList");
-		System.out.println("Does the file existed? "+ f.exists());
-		
-		if (f.exists())
-		{
+		System.out.println("Does the file existed? " + f.exists());
+
+		if (f.exists()) {
 			// Get a word from the file
 			BufferedReader br = new BufferedReader(new FileReader(f));
-			String word = br.readLine();
-			while (true)
-			{	// Create new game
-				Game g = new Game();
-				// Set the word to setWord
-				g.setWord(word);
+			String words = br.readLine();
+			String[] word = words.split(":");
+			// Create new game
+			Game g = new Game();
+			// Set the word to setWord
+			g.setWord(word[0]);
+			// Set hint
+			g.setHint(word[1]);
+			// Loop game until game is solve
+			while (!g.gameSolve()) {
 				// Print masked word
+				System.out.println("Word: " + g.maskWord());
 				// Print hint
+				System.out.println("Hint: " + g.getHint());
+				// Print lives
+				System.out.println("Lives: " + g.getLives());
 				// Ask for user input
 				String guess = user_input.nextLine();
 				// Set user input
 				g.setGuesses(guess);
-				// Un-mask user guess letter
-				System.out.println("Word: "+g.maskWord());
-				
+
 			}
 		}
-		
-		
+
 		return games;
 	}
-	
+
 	public static void main(String[] args) {
 		Controller rf = new Controller();
-		try 
-		{
+		try {
 			List<Game> games = rf.read();
-			for (Game game: games)      // repeat the loop and move on to the next word
+			for (Game game : games) // repeat the loop and move on to the next word
 			{
 				System.out.println(game);// can change with another variable
 			}
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- src/edu/ho/java/hangman_v2/Game.java
+++ src/edu/ho/java/hangman_v2/Game.java
@@ -5,72 +5,77 @@ import java.util.Scanner;
 
 // Process the input and check if it right
 //
-public class Game 
-{
-	private int lives;
-	private String guesses="";
+public class Game {
+	private int lives = 6;
+	private String guesses = "";
 	private String hint;
 	private String word;
-	
+
 	public int getLives() {
 		return lives;
 	}
+
 	public void setLives(int lives) {
 		this.lives = lives;
 	}
+
 	public String getGuesses() {
 		return guesses;
 	}
+
 	public void setGuesses(String guess) {
 		this.guesses += guess;
 	}
+
 	public String getHint() {
 		return hint;
 	}
+
 	public void setHint(String hint) {
 		this.hint = hint;
 	}
+
 	public String getWord() {
 		return word;
 	}
+
 	public void setWord(String word) {
 		this.word = word;
 	}
 
-	public String maskWord() 
-	{
-		String mask= "";
-		for (int i = 0; i < word.length(); i++) 
-		{
+	public String maskWord() {
+		String mask = "";
+		for (int i = 0; i < word.length(); i++) {
 			String currentChar = String.valueOf(word.charAt(i));
-			if (guesses.contains(currentChar))
-			{
+			if (guesses.contains(currentChar)) {
 				mask += currentChar;
-			}
-			else 
-			{
+			} else {
 				mask += "*";
 			}
-			
+
 		}
 		return mask;
 
 	}
-	
+
+	public void lives(String guess) {
+		if (!word.contains(guess)) {
+			// - 1 lives each wrong guess
+			lives--;
+		}
+	}
 	
 	public boolean gameSolve() {
 		return !maskWord().contains("*");
 	}
-		
+
 	public boolean dead() {
-		if (getLives() == 0) 
-		{
+		if (getLives() == 0) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
- 
-	}
-}
 
+	}
+
+}
